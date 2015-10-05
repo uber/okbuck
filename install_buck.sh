7209954d7886b30a456865ba5609dd53fb499b02
Piasy <xz4215@gmail.com>
2015-10-05 13:37:23 +0800

    fix ci 3rd try

---
+++ install_buck.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo add-apt-repository ppa:fkrull/deadsnakes -y && sudo apt-get update -qq && sudo apt-get install -y ant python2.7 && git clone https://github.com/facebook/buck.git && cd buck && ant
