73d242d58de0b59b73cf6ead463dea369e29752f
Piasy <xz4215@gmail.com>
2016-05-24 12:10:22 +0800

    minor update

--- buildSrc/src/main/resources/com/github/okbuilds/core/util/retrolambda/RetroLambda.sh
+++ buildSrc/src/main/resources/com/github/okbuilds/core/util/retrolambda/RetroLambda.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-DEPS=$(JARS=(`find gen-dir ! -name '*-abi.jar' ! -name '*#dex.dex.jar' ! -name '*,dex.dex.jar' -name '*.jar'`); IFS=:; echo "${JARS[*]}")
+DEPS=$(JARS=(`find gen-dir ! -name '*-abi.jar' ! -name '*dex.dex.jar' -name '*.jar'`); IFS=:; echo "${JARS[*]}")
 java -Dretrolambda.inputDir=$1 -Dretrolambda.classpath=$1:android-jar:$DEPS -jar retrolambda-jar
