1bde6d2d26626047f6d4d5d5f98299018e3dc1db
Gautam Korlam <gkorlam@uber.com>
2016-05-25 09:21:37 -0700

    Revert "add debuggable attr into merged manifest;"
    This reverts commit 4a397e830c5bfd0c59eae74b6847aa2c3f4635e5.

--- buildSrc/src/main/groovy/com/github/okbuilds/core/util/ProjectUtil.groovy
+++ buildSrc/src/main/groovy/com/github/okbuilds/core/util/ProjectUtil.groovy
@@ -67,12 +67,12 @@ final class ProjectUtil {
         switch (type) {
             case ProjectType.ANDROID_APP:
                 project.android.applicationVariants.collectEntries { BaseVariant variant ->
-                    [variant.name, new AndroidAppTarget(project, variant.name).extractMergedManifest()]
+                    [variant.name, new AndroidAppTarget(project, variant.name)]
                 }
                 break
             case ProjectType.ANDROID_LIB:
                 project.android.libraryVariants.collectEntries { BaseVariant variant ->
-                    [variant.name, new AndroidLibTarget(project, variant.name).extractMergedManifest()]
+                    [variant.name, new AndroidLibTarget(project, variant.name)]
                 }
                 break
             case ProjectType.JAVA_LIB:
@@ -102,7 +102,7 @@ final class ProjectUtil {
                             out.outputFile.equals(output)
                         }
                         if (variant != null) {
-                            result = new AndroidLibTarget(project, variant.name).extractMergedManifest()
+                            result = new AndroidLibTarget(project, variant.name)
                         }
                     }
                     break
