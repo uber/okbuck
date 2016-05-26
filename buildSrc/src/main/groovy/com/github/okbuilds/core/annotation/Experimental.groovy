438d99b227255951925a8694b11cb771cb203335
Gautam Korlam <gkorlam@uber.com>
2016-05-25 22:40:35 -0700

    Add experimental placeholder support

---
+++ buildSrc/src/main/groovy/com/github/okbuilds/core/annotation/Experimental.groovy
@@ -0,0 +1,10 @@
+package com.github.okbuilds.core.annotation
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
+@Retention(RetentionPolicy.SOURCE)
+@Target([ElementType.METHOD, ElementType.FIELD])
+@interface Experimental {}
