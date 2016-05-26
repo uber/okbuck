30786ae129ebb18b6b2f005d9441e0d9993c0567
Gautam Korlam <gkorlam@uber.com>
2016-05-25 22:46:51 -0700

    update documentation

--- buildSrc/src/main/groovy/com/github/okbuilds/okbuck/ExperimentalExtension.groovy
+++ buildSrc/src/main/groovy/com/github/okbuilds/okbuck/ExperimentalExtension.groovy
@@ -4,5 +4,7 @@ import org.apache.http.annotation.Experimental
 
 @Experimental
 class ExperimentalExtension {
+
+    // Enables support for manifest placeholders
     boolean placeholderSupport = false
 }
