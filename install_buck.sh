f91adddcc30d3482708874f7a7033ef6c9a8b7c7
Gautam Korlam <gkorlam@uber.com>
2016-05-25 22:55:49 -0700

    change install_buck.sh to clone a single branch

--- install_buck.sh
+++ install_buck.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-git clone --depth 1 https://github.com/OkBuilds/buck.git && cd buck && git checkout okbuck && ant
+
+git clone -b okbuck --single-branch --depth 1 https://github.com/OkBuilds/buck.git && cd buck && ant
