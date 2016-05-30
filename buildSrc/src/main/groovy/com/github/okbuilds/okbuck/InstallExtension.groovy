41f2d3c758a72bbd47fdd652e5d709ccf34459e2
Gautam Korlam <gkorlam@uber.com>
2016-05-30 11:05:03 -0500

    Install buck via gradle for travis

--- buildSrc/src/main/groovy/com/github/okbuilds/okbuck/InstallExtension.groovy
+++ buildSrc/src/main/groovy/com/github/okbuilds/okbuck/InstallExtension.groovy
@@ -8,7 +8,7 @@ class InstallExtension {
 
     String dir
     String gitUrl = ''
-    String sha = "origin/master"
+    String sha = "master"
 
     InstallExtension(Project project) {
         dir = new File(project.gradle.gradleUserHomeDir, "caches/okbuilds").absolutePath
