7a3957a150eada423b1eaae2d1c4703ccf38b70e
Gautam Korlam <gkorlam@uber.com>
2016-06-14 11:07:59 -0700

    Use buck's provided_deps

--- buildSrc/src/main/groovy/com/github/okbuilds/okbuck/composer/AndroidLibraryRuleComposer.groovy
+++ buildSrc/src/main/groovy/com/github/okbuilds/okbuck/composer/AndroidLibraryRuleComposer.groovy
@@ -18,6 +18,11 @@ final class AndroidLibraryRuleComposer extends AndroidBuckRuleComposer {
         deps.addAll(external(target.main.externalDeps))
         deps.addAll(targets(target.main.targetDeps))
 
+        Set<String> providedDeps = []
+        providedDeps.addAll(external(target.apt.externalDeps))
+        providedDeps.addAll(targets(target.apt.targetDeps))
+        providedDeps.removeAll(deps)
+
         target.main.targetDeps.each { Target targetDep ->
             if (targetDep instanceof AndroidTarget) {
                 targetDep.resources.each { AndroidTarget.ResBundle bundle ->
@@ -32,8 +37,8 @@ final class AndroidLibraryRuleComposer extends AndroidBuckRuleComposer {
         }
 
         return new AndroidLibraryRule(src(target), ["PUBLIC"], deps, target.main.sources,
-                target.manifest, target.annotationProcessors as List, aptDeps, aidlRuleNames,
-                appClass, target.sourceCompatibility, target.targetCompatibility,
+                target.manifest, target.annotationProcessors as List, aptDeps, providedDeps,
+                aidlRuleNames, appClass, target.sourceCompatibility, target.targetCompatibility,
                 postprocessClassesCommands, target.jvmArgs)
     }
 }
