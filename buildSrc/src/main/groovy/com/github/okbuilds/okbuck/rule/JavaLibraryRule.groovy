873ecd418a9019bfe2b964ccd99e3fccc8fb2dc8
Gautam Korlam <kageiit@users.noreply.github.com>
2016-06-29 17:00:46 -0700

    Support provided_deps for java rules

--- buildSrc/src/main/groovy/com/github/okbuilds/okbuck/rule/JavaLibraryRule.groovy
+++ buildSrc/src/main/groovy/com/github/okbuilds/okbuck/rule/JavaLibraryRule.groovy
@@ -4,11 +4,11 @@ final class JavaLibraryRule extends JavaRule {
 
     JavaLibraryRule(String name, List<String> visibility, List<String> deps,
                     Set<String> srcSet, Set<String> annotationProcessors,
-                    Set<String> annotationProcessorDeps, String sourceCompatibility,
+                    Set<String> aptDeps, Set<String> providedDeps, String sourceCompatibility,
                     String targetCompatibility, List<String> postprocessClassesCommands,
                     List<String> options) {
         super("java_library", name, visibility, deps, srcSet, annotationProcessors,
-                annotationProcessorDeps, sourceCompatibility, targetCompatibility,
+                aptDeps, providedDeps, sourceCompatibility, targetCompatibility,
                 postprocessClassesCommands, options)
     }
 }
