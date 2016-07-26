7ba742275301569cdb6e818dbc903fce50a2d10a
Gautam Korlam <gkorlam@uber.com>
2016-07-26 14:19:21 -0700

    WIP

--- buildSrc/src/main/groovy/com/github/okbuilds/core/annotation/Experimental.groovy
+++ buildSrc/src/main/groovy/com/github/okbuilds/core/annotation/Experimental.groovy
@@ -7,4 +7,10 @@ import java.lang.annotation.Target
 
 @Retention(RetentionPolicy.SOURCE)
 @Target([ElementType.TYPE, ElementType.METHOD, ElementType.FIELD])
-@interface Experimental {}
+@interface Experimental {
+
+    /**
+     * Enables generation of {@code robolectric_test} rules
+     */
+    boolean robolectric = false
+}
