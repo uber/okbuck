e759041935100c7136578e5be7bc758cc3a00b5b
Gautam Korlam <kageiit@users.noreply.github.com>
2016-09-16 15:14:17 -0500

    Reduce apt jar target dependencies

--- buildSrc/src/main/groovy/com/github/okbuilds/okbuck/composer/AptRuleComposer.groovy
+++ buildSrc/src/main/groovy/com/github/okbuilds/okbuck/composer/AptRuleComposer.groovy
@@ -1,6 +1,6 @@
 package com.github.okbuilds.okbuck.composer
 
-import com.github.okbuilds.core.model.Target
+import com.github.okbuilds.core.model.JavaTarget
 import com.github.okbuilds.okbuck.rule.AptRule
 
 final class AptRuleComposer extends JavaBuckRuleComposer {
@@ -9,7 +9,10 @@ final class AptRuleComposer extends JavaBuckRuleComposer {
         // no instance
     }
 
-    static AptRule compose(Target target) {
-        return new AptRule(aptJar(target), external(target.apt.externalDeps) as List)
+    static AptRule compose(JavaTarget target) {
+        Set<String> aptDeps = target.apt.externalDeps.findAll { String dep ->
+            dep.endsWith(".jar")
+        }
+        return new AptRule(aptJar(target), external(aptDeps) as List)
     }
 }
