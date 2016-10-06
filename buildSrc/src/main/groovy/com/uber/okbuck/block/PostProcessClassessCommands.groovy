3b85b950f54f0b64caad9e4c5cc80d34548ff143
Marcello Lelio Albano <marcello@uber.com>
2016-10-06 12:06:41 -0700

    Dependencies and android jar are now all provided as DEPS

--- buildSrc/src/main/groovy/com/uber/okbuck/block/PostProcessClassessCommands.groovy
+++ buildSrc/src/main/groovy/com/uber/okbuck/block/PostProcessClassessCommands.groovy
@@ -25,13 +25,11 @@ public class PostProcessClassessCommands {
     }
 
     public String buildCommand() {
-        String deps = "\$(JARS=(`find ${mGenDir} ! -name \"*-abi.jar\" ! -name \"*dex.dex.jar\" -name \"*.jar\"`); IFS=:; echo \"\${JARS[*]}\")"
-        String androidJar = mBootClasspath
-
+        String deps = "\$(JARS=(`find ${mGenDir} ! -name \"*-abi.jar\" ! -name \"*dex.dex.jar\" -name \"*.jar\"`); CLASSPATH=${mBootClasspath}; IFS=:; echo \"\${JARS[*]}\":\"\${CLASSPATH[*]}\")"
         StringBuilder sb = new StringBuilder()
         sb.append("\tpostprocess_classes_commands = [\n")
         mPostprocessClassesCommands.each {
-            String command -> sb.append("\t\t'DEPS=${deps} ANDROID_JAR=${androidJar} ${command}'\n,")
+            String command -> sb.append("\t\t'DEPS=${deps} ${command}'\n,")
         }
         sb.append("\t],")
         return sb.toString();
