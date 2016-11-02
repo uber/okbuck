8a94978eacde332b124d9575fa0ab3d123787eb8
Marcello Lelio Albano <marcello@uber.com>
2016-11-02 11:27:21 -0700

    Moved read dep file to be in line

--- buildSrc/src/main/resources/com/uber/okbuck/core/util/retrolambda/RetroLambda.sh
+++ buildSrc/src/main/resources/com/uber/okbuck/core/util/retrolambda/RetroLambda.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-DEPENDENCIES_CLASSPATH=("`cat ""$DEPENDENCIES_CLASSPATH_FILE""`")
-java -Dretrolambda.inputDir=$1 -Dretrolambda.classpath=$1:$DEPENDENCIES_CLASSPATH -jar retrolambda-jar
+java -Dretrolambda.inputDir=$1 -Dretrolambda.classpath=$1:`cat $DEPENDENCIES_CLASSPATH_FILE` -jar retrolambda-jar
