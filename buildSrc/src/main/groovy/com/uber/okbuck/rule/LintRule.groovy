98ccbe1ee5c618f4a00464ed7a5d4a1670094b2f
Gautam Korlam <kageiit@users.noreply.github.com>
2016-11-07 22:12:38 -0800

    Fix lint genrule to not use 'DEPS' (#230)

--- buildSrc/src/main/groovy/com/uber/okbuck/rule/LintRule.groovy
+++ buildSrc/src/main/groovy/com/uber/okbuck/rule/LintRule.groovy
@@ -6,24 +6,21 @@ final class LintRule extends BuckRule {
 
     private final String lintw
     private final Set<String> inputs
-    private final Set<String> customLintTargets
-    private final Set<String> classpathLintDeps
-    private final Set<String> locationLintDeps
+    private final Set<String> customLintRules
+    private final Set<String> lintDeps
     private final String lintTarget
 
     LintRule(String name,
              Set<String> inputs,
              String lintw,
-             Set<String> customLintTargets,
-             Set<String> classpathLintDeps,
-             Set<String> locationLintDeps,
+             Set<String> customLintRules,
+             Set<String> lintDeps,
              String lintTarget) {
         super("genrule", name)
         this.lintw = lintw
         this.inputs = inputs
-        this.customLintTargets = customLintTargets
-        this.classpathLintDeps = classpathLintDeps
-        this.locationLintDeps = locationLintDeps
+        this.customLintRules = customLintRules
+        this.lintDeps = lintDeps
         this.lintTarget = lintTarget
     }
 
@@ -37,34 +34,23 @@ final class LintRule extends BuckRule {
             printer.println("\t],")
         }
 
-        String lintDeps = [toClasspath(classpathLintDeps), toLocation(locationLintDeps)].findAll {
-            !it.empty
-        }.join(SEPARATOR)
-
         printer.println("\tout = '${name}_out',")
-        printer.println("\tbash = 'mkdir -p \$OUT; export LINT_DEPS=${lintDeps} ; ' \\")
-        printer.println("\t'export ANDROID_LINT_JARS=${toLocation(customLintTargets)} ; ' \\")
+        printer.println("\tbash = 'mkdir -p \$OUT ; ' \\")
+        if (lintDeps) {
+            printer.println("\t'export JAR_PATH=\"${toLocation(lintDeps)}\" ; ' \\")
+        }
+        if (customLintRules) {
+            printer.println("\t'export ANDROID_LINT_JARS=\"${toLocation(customLintRules)}\" ; ' \\")
+        }
         if (lintTarget) {
-            printer.println("\t'export MODULE_JAR=${toLocation(lintTarget)} ; ' \\")
+            printer.println("\t'export LINT_TARGET=${toLocation(lintTarget)} ; ' \\")
         }
         printer.println("\t'export OUTPUT_DIR=\$OUT ; ' \\")
         printer.println("\t'${toLocation(lintw)}',")
     }
 
-    static String toLintDeps(Set<String> targets) {
-        return (targets.collect { toClasspath(it) } as Set).join(SEPARATOR)
-    }
-
-    static String toClasspath(Set<String> targets) {
-        return (targets.collect { toClasspath(it) } as Set).join(SEPARATOR)
-    }
-
     static String toLocation(Set<String> targets) {
         return (targets.collect { toLocation(it) } as Set).join(SEPARATOR)
-    }
-
-    static String toClasspath(String target) {
-        return "\$(classpath ${target})"
     }
 
     static String toLocation(String target) {
