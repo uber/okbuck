6b174b9dac4351d57c1b39b6fd4635914839b3bf
Gautam Korlam <gkorlam@uber.com>
2016-11-08 01:36:01 -0800

    Simplify okbuck generate lint genrules

--- buildSrc/src/main/groovy/com/uber/okbuck/composer/LintRuleComposer.groovy
+++ buildSrc/src/main/groovy/com/uber/okbuck/composer/LintRuleComposer.groovy
@@ -1,51 +1,123 @@
 package com.uber.okbuck.composer
 
+import com.uber.okbuck.core.model.AndroidTarget
 import com.uber.okbuck.core.model.JavaTarget
 import com.uber.okbuck.core.model.ProjectType
 import com.uber.okbuck.core.model.Target
 import com.uber.okbuck.core.util.LintUtil
 import com.uber.okbuck.core.util.ProjectUtil
 import com.uber.okbuck.extension.LintExtension
-import com.uber.okbuck.generator.LintWrapperGenerator
-import com.uber.okbuck.rule.LintRule
+import com.uber.okbuck.rule.GenRule
 
 final class LintRuleComposer extends JavaBuckRuleComposer {
+
+    static final String SEPARATOR = ':'
 
     private LintRuleComposer() {
         // no instance
     }
 
-    static LintRule compose(JavaTarget target) {
-        Set<String> inputs = []
+    static GenRule compose(JavaTarget target) {
+        List<String> inputs = []
         if (target.lintOptions.lintConfig != null && target.lintOptions.lintConfig.exists()) {
             inputs.add(LintUtil.getLintwConfigRule(target.project, target.lintOptions.lintConfig))
         }
 
-        LintExtension lintExtension = target.rootProject.okbuck.lint
-        LintWrapperGenerator.generate(target, lintExtension.jvmArgs)
-
-        Set<Target> customLintTargets = target.lint.targetDeps.findAll {
+        List<Target> customLintTargets = target.lint.targetDeps.findAll {
             (it instanceof JavaTarget) && (it.hasLintRegistry())
-        }
+        } as List
 
-        Set<String> customLintRules = [] as Set
+        List<String> customLintRules = []
         customLintRules.addAll(external(target.main.packagedLintJars))
-        customLintRules.addAll(targets(customLintTargets))
+        customLintRules.addAll(targets(customLintTargets as Set))
 
-        Set<String> lintDeps = [] as Set
+        List<String> lintDeps = []
         lintDeps.addAll(LintUtil.LINT_DEPS_RULE)
         customLintTargets.each {
             if (ProjectUtil.getType(it.project) == ProjectType.JAVA_APP) {
-                lintDeps.addAll(binTargets(it))
+                lintDeps.add(binTargets(it))
             }
         }
 
-        return new LintRule(
+        List<String> lintCmds = []
+        if (customLintRules) {
+            lintCmds.add("export ANDROID_LINT_JARS=\"${toLocation(customLintRules)}\";")
+        }
+        lintCmds += ["mkdir -p \$OUT;", "exec java", "-Djava.awt.headless=true"]
+
+        LintExtension lintExtension = target.rootProject.okbuck.lint
+        if (lintExtension.jvmArgs) {
+            lintCmds.add(lintExtension.jvmArgs)
+        }
+        if (lintDeps) {
+            lintCmds.add("-classpath ${toLocation(lintDeps)}")
+        }
+        lintCmds.add("com.android.tools.lint.Main")
+
+        if (!target.main.sources.empty) {
+            lintCmds.add("--classpath ${toLocation(":${src(target)}")}")
+        }
+        if (target.lintOptions.abortOnError) {
+            lintCmds.add("--exitcode")
+        }
+        if (target.lintOptions.absolutePaths) {
+            lintCmds.add("--fullpath")
+        }
+        if (target.lintOptions.quiet) {
+            lintCmds.add("--quiet")
+        }
+        if (target.lintOptions.checkAllWarnings) {
+            lintCmds.add("-Wall")
+        }
+        if (target.lintOptions.ignoreWarnings) {
+            lintCmds.add("--nowarn")
+        }
+        if (target.lintOptions.warningsAsErrors) {
+            lintCmds.add("-Werror")
+        }
+        if (target.lintOptions.noLines) {
+            lintCmds.add("--nolines")
+        }
+        if (target.lintOptions.disable) {
+            lintCmds.add("--disable ${target.lintOptions.disable.join(',')}")
+        }
+        if (target.lintOptions.enable) {
+            lintCmds.add("--enable ${target.lintOptions.enable.join(',')}")
+        }
+        if (target.lintOptions.lintConfig && target.lintOptions.lintConfig.exists()) {
+            lintCmds.add("--config ${target.lintOptions.lintConfig.absolutePath}")
+        }
+        if (target.lintOptions.xmlReport) {
+            lintCmds.add('--xml "\$OUT/lint-results.xml"')
+        }
+        if (target.lintOptions.htmlReport) {
+            lintCmds.add('--html "\$OUT/lint-results.html"')
+        }
+        target.main.sources.each { String sourceDir ->
+            lintCmds.add("--sources ${new File(target.project.projectDir, sourceDir).absolutePath}")
+        }
+        if (target instanceof AndroidTarget) {
+            target.getResources().each { AndroidTarget.ResBundle bundle ->
+                if (bundle.resDir) {
+                    lintCmds.add("--resources ${new File(target.project.projectDir, bundle.resDir).absolutePath}")
+                }
+            }
+
+            // Project root is at okbuck generated manifest for this target
+            lintCmds.add("${new File(target.project.projectDir, target.manifest).parentFile.absolutePath}")
+        }
+
+        return new GenRule(
                 lint(target),
                 inputs,
-                LintUtil.getLintwRule(target),
-                customLintRules,
-                lintDeps,
-                target.main.sources.empty ? null : ":${src(target)}")
+                lintCmds)
+    }
+
+    static String toLocation(List<String> targets) {
+        return (targets.collect { toLocation(it) }).join(SEPARATOR)
+    }
+
+    static String toLocation(String target) {
+        return "\$(location ${target})"
     }
 }
--- buildSrc/src/main/groovy/com/uber/okbuck/core/util/LintUtil.groovy
+++ buildSrc/src/main/groovy/com/uber/okbuck/core/util/LintUtil.groovy
@@ -15,17 +15,11 @@ class LintUtil {
 
     static final String LINT_GROUP = "com.android.tools.lint"
     static final String LINT_MODULE = "lint"
-    static final String LINTW_LOCATION = ".okbuck/lintw"
-    static final String LINTW_BUCK_FILE = "lint/LINTW_BUCK_FILE"
-    static final String LINTW_TEMPLATE = "lint/LINTW_TEMPLATE"
-    static final File LINTW_BUCK = new File("${LINTW_LOCATION}/BUCK")
 
     static final String LINT_DEPS_CONFIG = "${OkBuckGradlePlugin.BUCK_LINT}_deps"
-    static final String LINT_DEPS_CACHE = "${OkBuckGradlePlugin.DEFAULT_CACHE_PATH}/lintDeps"
-    static final String LINT_DEPS_RULE = "//${LINT_DEPS_CACHE}:okbuck_lint_deps"
-    static final String LINT_DEPS_BUCK_FILE = "lint/LINT_DEPS_BUCK_FILE"
-
-    static final String LINT_CACHE = "${OkBuckGradlePlugin.DEFAULT_CACHE_PATH}/lint"
+    static final String LINT_DEPS_CACHE = "${OkBuckGradlePlugin.DEFAULT_CACHE_PATH}/lint"
+    static final String LINT_DEPS_RULE = "//${LINT_DEPS_CACHE}:okbuck_lint"
+    static final String LINT_DEPS_BUCK_FILE = "lint/BUCK_FILE"
 
     private LintUtil() {}
 
@@ -61,36 +55,12 @@ class LintUtil {
         lintDepsScope.getExternalDeps()
     }
 
-    static File createLintw(JavaTarget target) {
-        File lintw = new File(target.rootProject.projectDir, getLintwPath(target))
-        lintw.parentFile.mkdirs()
-        lintw.createNewFile()
-
-        FileUtil.copyResourceToProject(LINTW_TEMPLATE, lintw)
-        if (!LINTW_BUCK.exists()) {
-            FileUtil.copyResourceToProject(LINTW_BUCK_FILE, LINTW_BUCK)
-        }
-        return lintw
-    }
-
-    static String getLintwScriptName(JavaTarget target) {
-        return "lintw_${target.identifier.replaceAll(':', '_')}_${target.name}"
-    }
-
-    static String getLintwPath(JavaTarget target) {
-        return "${LINTW_LOCATION}/${getLintwScriptName(target)}"
-    }
-
-    static String getLintwRule(JavaTarget target) {
-        return "//${LINTW_LOCATION}:${getLintwScriptName(target)}"
-    }
-
     static String getLintwConfigName(Project project, File config) {
         return FileUtil.getRelativePath(project.rootDir, config).replaceAll('/', '_')
     }
 
     static String getLintwConfigRule(Project project, File config) {
-        File configFile = new File("${LINTW_LOCATION}/${getLintwConfigName(project, config)}")
+        File configFile = new File("${LINT_DEPS_CACHE}/${getLintwConfigName(project, config)}")
         if (!configFile.exists() || !FileUtils.contentEquals(configFile, config)) {
             if (configFile.exists()) {
                 configFile.delete()
@@ -99,17 +69,7 @@ class LintUtil {
             }
             Files.copy(config.toPath(), configFile.toPath())
         }
-        return "//${LINTW_LOCATION}:${getLintwConfigName(project, config)}"
-    }
-
-    static DependencyCache getLintCache(Project project) {
-        return new DependencyCache(project.rootProject, LINT_CACHE) {
-
-            @Override
-            boolean isValid(File dep) {
-                return dep.name.endsWith(".jar")
-            }
-        }
+        return "//${LINT_DEPS_CACHE}:${getLintwConfigName(project, config)}"
     }
 
     static DependencyCache getLintDepsCache(Project project) {
