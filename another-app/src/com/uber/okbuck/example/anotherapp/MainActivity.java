20bf3639a92a4b9b7422c1ff975a8e25e86f9053
Chris Lacy <gdaychris@gmail.com>
2017-01-05 08:50:49 +1000

    Example of custom Application class not being created when using buck
    Run another-app via a gradle build. Observe the “D/MainActivity:
    MainActivity.onCreate()
    application:com.uber.okbuck.example.anotherapp.MainApplication” log
    message.
    
    When running with buck (via ./buckw install --run another-appDebug),
    the log message is “D/MainActivity: MainActivity.onCreate()
    application:android.app.Application”

--- another-app/src/com/uber/okbuck/example/anotherapp/MainActivity.java
+++ another-app/src/com/uber/okbuck/example/anotherapp/MainActivity.java
@@ -21,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("MainActivity", "MainActivity.onCreate() application:"
+                + getApplication().getClass().getName());
+
+
         mTvTest = (TextView) findViewById(R.id.mTvTest);
         mTvTest.setOnClickListener(new View.OnClickListener() {
             @Override
