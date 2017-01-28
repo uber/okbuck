108f66f158b56c65cabb33e6a4e1163e1090b861
Gautam Korlam <gkorlam@uber.com>
2017-01-28 01:37:11 -0800

    Bump default retrolambda version to 2.5.0

--- buildSrc/src/main/java/com/uber/okbuck/extension/RetrolambdaExtension.java
+++ buildSrc/src/main/java/com/uber/okbuck/extension/RetrolambdaExtension.java
@@ -9,7 +9,7 @@ public class RetrolambdaExtension {
     /**
      * Retrolambda version. Defaults to '2.3.0'
      */
-    public String version = "2.3.0";
+    public String version = "2.5.0";
 
     /**
      * Jvm arguments when running retrolambda
