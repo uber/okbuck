2d701e10a0236aaf62c52743d3226a28c366cb2e
Ravi Agarwal <raviagarwal7@users.noreply.github.com>
2017-04-22 14:39:44 -0700

    Add support for downloading 22-25 API robolectric runtime dependencies. (#427)
    Add below to DEFS file to support robolectric 3.3.2
    
    ```
    original_robolectric_test = robolectric_test
    def robolectric_test(
        name,
        force_final_resource_ids=false,
        **kwargs
        ):
        original_robolectric_test(
          name=name,
          force_final_resource_ids=false,
          **kwargs
        )
    ```
    Buck changes will be merged in - https://github.com/facebook/buck/pull/1305

--- buildSrc/src/main/java/com/uber/okbuck/core/util/RobolectricUtil.java
+++ buildSrc/src/main/java/com/uber/okbuck/core/util/RobolectricUtil.java
@@ -32,7 +32,9 @@ public final class RobolectricUtil {
             Configuration runtimeApi = project.getConfigurations().maybeCreate(
                     ROBOLECTRIC_RUNTIME + "_" + api.name());
             project.getDependencies().add(runtimeApi.getName(), api.androidJar);
-            project.getDependencies().add(runtimeApi.getName(), api.shadowsJar);
+            if (api.shadowsJar != null) {
+                project.getDependencies().add(runtimeApi.getName(), api.shadowsJar);
+            }
             runtimeDeps.add(runtimeApi);
         }
 
@@ -52,7 +54,11 @@ public final class RobolectricUtil {
         API_17("org.robolectric:android-all:4.2.2_r1.2-robolectric-0", "org.robolectric:shadows-core:3.0:17"),
         API_18("org.robolectric:android-all:4.3_r2-robolectric-0", "org.robolectric:shadows-core:3.0:18"),
         API_19("org.robolectric:android-all:4.4_r1-robolectric-1", "org.robolectric:shadows-core:3.0:19"),
-        API_21("org.robolectric:android-all:5.0.0_r2-robolectric-1", "org.robolectric:shadows-core:3.0:21");
+        API_21("org.robolectric:android-all:5.0.0_r2-robolectric-1", "org.robolectric:shadows-core:3.0:21"),
+        API_22("org.robolectric:android-all:5.1.1_r9-robolectric-1", null),
+        API_23("org.robolectric:android-all:6.0.1_r3-robolectric-0", null),
+        API_24("org.robolectric:android-all:7.0.0_r1-robolectric-0", null),
+        API_25("org.robolectric:android-all:7.1.0_r7-robolectric-0", null);
 
         private final String androidJar;
         private final String shadowsJar;
