406e95e4ab45b7647856f72008be35bb70346321
Nelson Osacky <nelson@osacky.com>
2017-05-04 10:21:13 -0700

    Add empty resource rule if using resource union. (#438)
    This ensures that an R file is created for that module even if there are
    no resources. Gradle always generates an R file for each module even if
    there are no resources.

---
+++ libraries/emptyResourceLibrary/src/main/java/com/uber/okbuck/library/empty/resources/Test.java
@@ -0,0 +1,10 @@
+package com.uber.okbuck.library.empty.resources;
+
+import android.content.Context;
+
+public class Test {
+
+  public String foo(Context context) {
+    return context.getString(R.string.empty_release_string);
+  }
+}
