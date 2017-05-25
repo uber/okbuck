0f11945cbf44dde4a98c2ea05bf40fafee88a087
Marcin <marcin.kwiatkowski@hotmail.com>
2017-05-24 19:12:51 -0700

    only declare tools if needed

--- buildSrc/src/main/groovy/com/uber/okbuck/core/model/android/AndroidTarget.groovy
+++ buildSrc/src/main/groovy/com/uber/okbuck/core/model/android/AndroidTarget.groovy
@@ -248,17 +248,18 @@ abstract class AndroidTarget extends JavaLibTarget {
         } else {
             manifestXml.'uses-sdk'.replaceNode(sdkNode)
         }
-
+        
         def builder = new StreamingMarkupBuilder()
         builder.setUseDoubleQuotes(true)
         return (builder.bind {
-            mkp.declareNamespace(tools:'http://schemas.android.com/tools')
+            mkp.declareNamespace(android:'http://schemas.android.com/apk/res/android')
+            if (manifestXml.lookupNamespace("tools")) {
+                mkp.declareNamespace(tools:'http://schemas.android.com/tools')
+            }
             mkp.yield manifestXml
         } as String)
                 .replaceAll('\\{http://schemas.android.com/apk/res/android\\}', 'android:')
                 .replaceAll('\\{http://schemas.android.com/tools\\}', 'tools:')
-                .replaceAll('xmlns:android="http://schemas.android.com/apk/res/android"', '')
-                .replaceFirst('<manifest ', '<manifest xmlns:android="http://schemas.android.com/apk/res/android" ')
     }
 
     private void ensureManifest() {
