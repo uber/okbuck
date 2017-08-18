af51fe96788c61ef8a2d1548b3e8537380970141
James Barr <jbarr21@users.noreply.github.com>
2017-08-17 21:18:57 -0700

    Add sample custom view (#506)

---
+++ another-app/src/com/uber/okbuck/example/anotherapp/CustomView.java
@@ -0,0 +1,19 @@
+package com.uber.okbuck.example.anotherapp;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+public class CustomView extends AppCompatTextView {
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
