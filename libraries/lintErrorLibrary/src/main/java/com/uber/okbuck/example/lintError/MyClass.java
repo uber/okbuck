298063e0e878cb570439ee7170c20d2913d76087
James Barr <jbarr@uber.com>
2017-10-13 23:23:16 -0700

    NewApi Lint test and bump to 26rc1

--- libraries/lintErrorLibrary/src/main/java/com/uber/okbuck/example/lintError/MyClass.java
+++ libraries/lintErrorLibrary/src/main/java/com/uber/okbuck/example/lintError/MyClass.java
@@ -1,8 +1,23 @@
 package com.uber.okbuck.example.lintError;
 
+import android.util.Log;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class MyClass {
 
-    public void getSystemTime() {
+    public void testCustomLint_usingSystemTime_shouldFail() {
         System.currentTimeMillis();
+    }
+
+    public void testNewApiSdkLint_usingStreamsApi_shouldFail() {
+        Arrays.asList(1, 2, 3).stream()
+                .forEach(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer num) {
+                        Log.d("TAG", "number is: " + String.valueOf(num));
+                    }
+                });
     }
 }
