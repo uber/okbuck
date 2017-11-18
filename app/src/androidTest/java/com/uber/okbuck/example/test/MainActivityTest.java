4d230faaa8150e64a8d52d8faaf22bb37ca2a5d6
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2017-11-17 18:23:18 -0800

    Use d8 dexer for instrumentation apk.

--- app/src/androidTest/java/com/uber/okbuck/example/test/MainActivityTest.java
+++ app/src/androidTest/java/com/uber/okbuck/example/test/MainActivityTest.java
@@ -10,10 +10,12 @@ import org.junit.runner.RunWith;
 
 import com.uber.okbuck.example.R;
 import android.support.test.runner.AndroidJUnit4;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -25,5 +27,11 @@ public class MainActivityTest {
     @Test
     public void checkTextDisplayed() {
         onView(withId(R.id.mTextView2)).check(matches(withText("test in app")));
+    }
+
+    @Test
+    public void java8LambdaCompilesAndWorksInInstrumentationApk() {
+        Runnable runnable = () -> assertEquals(3, 2 + 1);
+        runnable.run();
     }
 }
