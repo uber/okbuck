abbad058bbe78f2740e47107f6ddaa20adac9192
Gautam Korlam <kageiit@users.noreply.github.com>
2018-01-26 15:29:47 +0100

    Fix Travis CI Build (#573)

--- app/src/androidTest/java/com/uber/okbuck/example/test/MainActivityTest.java
+++ app/src/androidTest/java/com/uber/okbuck/example/test/MainActivityTest.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertEquals;
 
