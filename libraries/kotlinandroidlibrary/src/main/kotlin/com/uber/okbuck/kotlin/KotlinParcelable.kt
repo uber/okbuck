95a38bcbad7f1db927fb3173e641cc747f0fad78
Gautam Korlam <gkorlam@uber.com>
2018-05-01 13:57:22 -0700

    Fix lint

--- libraries/kotlinandroidlibrary/src/main/kotlin/com/uber/okbuck/kotlin/KotlinParcelable.kt
+++ libraries/kotlinandroidlibrary/src/main/kotlin/com/uber/okbuck/kotlin/KotlinParcelable.kt
@@ -1,7 +1,9 @@
 package com.uber.okbuck.kotlin
 
+import android.annotation.SuppressLint
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+@SuppressLint("ParcelCreator") // https://youtrack.jetbrains.com/issue/KT-19300
 @Parcelize
 class KotlinParcelable(val name: String) : Parcelable
