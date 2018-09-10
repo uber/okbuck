04d993e60c5de06bb8a6d60164cb75f7f7670bf6
Gautam Korlam <gkorlam@uber.com>
2018-09-09 20:08:40 -0700

    Fix custom lint

--- libraries/customLintLibrary/src/main/java/com/uber/lint/LintRegistry.java
+++ libraries/customLintLibrary/src/main/java/com/uber/lint/LintRegistry.java
@@ -1,6 +1,7 @@
 package com.uber.lint;
 
 import com.android.tools.lint.client.api.IssueRegistry;
+import com.android.tools.lint.detector.api.ApiKt;
 import com.android.tools.lint.detector.api.Issue;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
@@ -10,5 +11,9 @@ public class LintRegistry extends IssueRegistry {
   @Override
   public List<Issue> getIssues() {
     return ImmutableList.of(SystemCurrentTimeMillisDetector.ISSUE);
+  }
+
+  @Override public int getApi() {
+    return ApiKt.CURRENT_API;
   }
 }
