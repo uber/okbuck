b5cd2b1f675fac2f4376d7fc8e8e431fdf06c831
Zac Sweers <pandanomic@gmail.com>
2018-12-31 00:29:18 -0800

    Add test projects

---
+++ libraries/customLintLibrarySpi/src/main/java/com/uber/lint/LintRegistrySpi.java
@@ -0,0 +1,19 @@
+package com.uber.lint;
+
+import com.android.tools.lint.client.api.IssueRegistry;
+import com.android.tools.lint.detector.api.ApiKt;
+import com.android.tools.lint.detector.api.Issue;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+
+public class LintRegistrySpi extends IssueRegistry {
+
+  @Override
+  public List<Issue> getIssues() {
+    return ImmutableList.of(SystemCurrentTimeMillisDetector.ISSUE, ColorDetector.ISSUE);
+  }
+
+  @Override public int getApi() {
+    return ApiKt.CURRENT_API;
+  }
+}
---
+++ libraries/customLintLibrarySpiAuto/src/main/java/com/uber/lint/LintRegistrySpiAuto.java
@@ -0,0 +1,21 @@
+package com.uber.lint;
+
+import com.android.tools.lint.client.api.IssueRegistry;
+import com.android.tools.lint.detector.api.ApiKt;
+import com.android.tools.lint.detector.api.Issue;
+import com.google.auto.service.AutoService;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+
+@AutoService(IssueRegistry.class)
+public class LintRegistrySpiAuto extends IssueRegistry {
+
+  @Override
+  public List<Issue> getIssues() {
+    return ImmutableList.of(ColorDetector.ISSUE);
+  }
+
+  @Override public int getApi() {
+    return ApiKt.CURRENT_API;
+  }
+}
