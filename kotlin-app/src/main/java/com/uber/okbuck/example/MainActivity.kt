3060ec04b63801dfa8033f8e956acc85f8b98857
James Coggan <jamescoggan@Jamess-MacBook-Pro.local>
2019-01-18 22:15:26 +0000

    Added the shared ui Android library and used the resources in the kotlin-app

--- kotlin-app/src/main/java/com/uber/okbuck/example/MainActivity.kt
+++ kotlin-app/src/main/java/com/uber/okbuck/example/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.uber.okbuck.example
 
+import android.graphics.ColorFilter
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import com.uber.okbuck.example.sqldelightmodel.GithubRepo
 import dagger.android.support.DaggerAppCompatActivity
@@ -12,10 +14,15 @@ class MainActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var analytics: Analytics
 
+    lateinit var smile : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        smile = findViewById(R.id.icon_smile)
+        smile.tag = "test"
 
         fab.setOnClickListener {
             analytics.sendAnalyticsEvent("FAB Clicked")
