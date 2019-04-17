c00f5b4b69776209e2a25bdf681234dc339ab06d
Ravi Agarwal <ravi@uber.com>
2019-04-17 22:32:44 +0530

    feedback

--- buildSrc/src/main/java/com/uber/okbuck/extension/ResolutionAction.java
+++ buildSrc/src/main/java/com/uber/okbuck/extension/ResolutionAction.java
@@ -2,17 +2,11 @@ package com.uber.okbuck.extension;
 
 public enum ResolutionAction {
   // Use latest version of the external dependency
-  LATEST("latest"),
+  LATEST,
 
   // Use all resolved versions of the external dependency
-  ALL("all"),
+  ALL,
 
   // Use single resolved version of the external dependency and fail if multiple versions are found
-  SINGLE("single");
-
-  private final String action;
-
-  ResolutionAction(String action) {
-    this.action = action;
-  }
+  SINGLE;
 }
