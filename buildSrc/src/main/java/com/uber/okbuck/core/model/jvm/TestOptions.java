ff13f4b556444d0e07c6efa9381314276d19fe18
Ravi Agarwal <ravi@uber.com>
2019-07-31 15:26:52 +0200

    Ensure that test env dict is printed in sorted manner

--- buildSrc/src/main/java/com/uber/okbuck/core/model/jvm/TestOptions.java
+++ buildSrc/src/main/java/com/uber/okbuck/core/model/jvm/TestOptions.java
@@ -2,22 +2,23 @@ package com.uber.okbuck.core.model.jvm;
 
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public final class TestOptions {
 
   private final List<String> jvmArgs;
-  private final Map<String, Object> env;
+  private final TreeMap<String, Object> env;
 
   public TestOptions(List<String> jvmArgs, Map<String, Object> env) {
     this.jvmArgs = jvmArgs;
-    this.env = env;
+    this.env = new TreeMap<>(env);
   }
 
   public List<String> getJvmArgs() {
     return jvmArgs;
   }
 
-  public Map<String, Object> getEnv() {
+  public TreeMap<String, Object> getEnv() {
     return env;
   }
 }
