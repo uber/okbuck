7f878c047cb34c3f289c6d11a2d0733673cf37cc
Thales Machado <thales@uber.com>
2019-09-11 13:59:04 +0200

    Added custom rules for integration tests

--- buildSrc/src/main/java/com/uber/okbuck/core/model/base/ProjectType.java
+++ buildSrc/src/main/java/com/uber/okbuck/core/model/base/ProjectType.java
@@ -4,21 +4,23 @@ public enum ProjectType {
   ANDROID_APP,
   ANDROID_LIB,
   JAVA_LIB,
-  GROOVY_LIB(RuleType.GROOVY_LIBRARY, RuleType.GROOVY_TEST),
-  KOTLIN_LIB(RuleType.KOTLIN_LIBRARY, RuleType.KOTLIN_TEST),
-  SCALA_LIB(RuleType.SCALA_LIBRARY, RuleType.SCALA_TEST),
+  GROOVY_LIB(RuleType.GROOVY_LIBRARY, RuleType.GROOVY_TEST, RuleType.GROOVY_INTEGRATION_TEST),
+  KOTLIN_LIB(RuleType.KOTLIN_LIBRARY, RuleType.KOTLIN_TEST, RuleType.KOTLIN_INTEGRATION_TEST),
+  SCALA_LIB(RuleType.SCALA_LIBRARY, RuleType.SCALA_TEST, RuleType.SCALA_INTEGRATION_TEST),
   UNKNOWN;
 
   private final RuleType mainRuleType;
   private final RuleType testRuleType;
+  private final RuleType integrationTestRuleType;
 
   ProjectType() {
-    this(RuleType.JAVA_LIBRARY, RuleType.JAVA_TEST);
+    this(RuleType.JAVA_LIBRARY, RuleType.JAVA_TEST, RuleType.JAVA_INTEGRATION_TEST);
   }
 
-  ProjectType(RuleType mainRuleType, RuleType testRuleType) {
+  ProjectType(RuleType mainRuleType, RuleType testRuleType, RuleType integrationTestRuleType) {
     this.mainRuleType = mainRuleType;
     this.testRuleType = testRuleType;
+    this.integrationTestRuleType = integrationTestRuleType;
   }
 
   public RuleType getMainRuleType() {
@@ -27,5 +29,9 @@ public enum ProjectType {
 
   public RuleType getTestRuleType() {
     return testRuleType;
+  }
+
+  public RuleType getIntegrationTestRuleType() {
+    return integrationTestRuleType;
   }
 }
