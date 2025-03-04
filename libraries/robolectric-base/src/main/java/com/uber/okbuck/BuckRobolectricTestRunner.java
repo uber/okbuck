7a8a3dd15ed4964bab2c451eee64d7ef2d1d34d2
Gaëtan Muller <m.gaetan89@gmail.com>
2025-03-04 09:35:58 +0100

    Migrate Robolectric's deprecated code to the recommended alternatives
    - Add `:libraries:robolectric-base` to the list of Gradle modules to load.
    - Add missing jUnit dependency to `:libraries:robolectric-base`.
    - Replace `BuckRobolectricTestRunner#getAppManifest(Config)` with `BuckRobolectricTestRunner#getManifestFactory(Config)`. The former is no longer public, and the recommended alternative is to use a custom `ManifestFactory`.
    - Create `BuckManifestFactory` to provide custom information for the creation of `AndroidManifest`.

--- libraries/robolectric-base/src/main/java/com/uber/okbuck/BuckRobolectricTestRunner.java
+++ libraries/robolectric-base/src/main/java/com/uber/okbuck/BuckRobolectricTestRunner.java
@@ -1,24 +1,14 @@
 package com.uber.okbuck;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.ListIterator;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import org.robolectric.manifest.AndroidManifest;
-import org.robolectric.res.Fs;
-import org.robolectric.res.FsFile;
-import org.robolectric.res.ResourcePath;
+import org.robolectric.internal.ManifestFactory;
 
 /**
  * The example test runner only runs robolectric tests with buck. If you want to run unit tests with
  * both gradle and buck, you can use the example test runner and create a hybrid with robolectric's
- * {@link org.robolectric.RobolectricGradleTestRunner}.
+ * {@link org.robolectric.RobolectricTestRunner}.
  */
 public class BuckRobolectricTestRunner extends RobolectricTestRunner {
 
@@ -30,39 +20,14 @@ public class BuckRobolectricTestRunner extends RobolectricTestRunner {
   }
 
   @Override
-  protected AndroidManifest getAppManifest(Config config) {
+  protected ManifestFactory getManifestFactory(Config config) {
     String buckManifest = System.getProperty(ROBOLECTRIC_MANIFEST);
     String buckResourcesProperty = System.getProperty(ROBOLECTRIC_RESOURCE_DIRECTORIES);
 
     if (buckManifest != null && buckResourcesProperty != null) {
-      final List<String> buckResources =
-          Arrays.asList(buckResourcesProperty.split(File.pathSeparator));
-
-      final FsFile res = Fs.fileFromPath(buckResources.get(buckResources.size() - 1));
-      final FsFile assets = Fs.fileFromPath(buckResources.get(buckResources.size() - 1));
-      final FsFile manifest = Fs.fileFromPath(buckManifest);
-
-      return new AndroidManifest(manifest, res, assets, config.packageName()) {
-
-        @Override
-        public List<ResourcePath> getIncludedResourcePaths() {
-          Collection<ResourcePath> resourcePaths = new LinkedHashSet<>();
-          resourcePaths.add(super.getResourcePath());
-
-          ListIterator<String> it = buckResources.listIterator(buckResources.size());
-          while (it.hasPrevious()) {
-            resourcePaths.add(
-                new ResourcePath(
-                    getRClass(),
-                    getPackageName(),
-                    Fs.fileFromPath(it.previous()),
-                    getAssetsDirectory()));
-          }
-          return new ArrayList<>(resourcePaths);
-        }
-      };
-    } else {
-      return null;
+      return new BuckManifestFactory(buckManifest, buckResourcesProperty);
     }
+
+    return super.getManifestFactory(config);
   }
 }
